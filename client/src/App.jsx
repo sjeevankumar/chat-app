c19b7584d54b7b5d1c7767de30a24e3b092c69fc
sjeevankumar <sjeevankumar89@gmail.com>
2024-06-06 16:24:45 +0530

    feat:initial client setup

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Register from './pages/Register'
+import Login from './pages/Login'
+import Chat from './pages/Chat'
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Chat />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
