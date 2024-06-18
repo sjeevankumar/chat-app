dd55889f7b168630f10bd32862f991264f377e0a
sjeevankumar <sjeevankumar89@gmail.com>
2024-06-18 13:54:03 +0530

    feat:complete set avatat server

--- server/src/model/userModel.js
+++ server/src/model/userModel.js
@@ -23,7 +23,7 @@ const userSchema = new mongoose.Schema({
         type:Boolean,
         default:false
     },
-    avatarimage:{
+    avatarImage:{
         type:String,
         default:""
     }
